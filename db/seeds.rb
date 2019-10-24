befda498ce8ff98e7ff19bc9100911a854c5ab8a
Lea-Brml <l.bramoulle@hotmail.fr>
2019-10-24 21:32:56 +0200

    ozehrz

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+
+6.times do |index|
+
+  City.create(city_name: Faker::Address.city)
+
+end
