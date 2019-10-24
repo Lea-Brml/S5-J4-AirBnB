daf1da0c089b43658abf49fb54dcf6558c35cef6
Lea-Brml <l.bramoulle@hotmail.fr>
2019-10-24 22:18:39 +0200

    bete au vent

---
+++ db/migrate/20191024200041_lien.rb
@@ -0,0 +1,6 @@
+class Lien < ActiveRecord::Migration[5.2]       #migration permettant de créer non clés et de lier les tables grâce à l'id de la city
+  def change
+    add_reference :strolls, :city, foreign_key: true
+    add_reference :dogs, :city, foreign_key: true
+  end
+end
